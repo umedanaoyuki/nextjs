52f9b7afca7c111ea808b8a7a825c9572ce7666a
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-02 15:33:29 +0900

    ルーティングについて

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
+
+// indexページ
 export default function Home() {
   return (
     <main className={styles.main}>
---
+++ app/posts/page.js
@@ -0,0 +1,5 @@
+// localhost:3000/posts というルーティング
+// pages.jsという名前は変わらない
+export default function Page() {
+    return <h1>First Post</h1>;
+}
