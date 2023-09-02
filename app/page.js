34f2e403b570666ef55585a4b5d3ace5573990f7
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-02 15:23:29 +0900

    トップページの変更

--- app/page.js
+++ app/page.js
@@ -28,16 +28,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <p>Learn Next.js</p>
 
       <div className={styles.grid}>
         <a
